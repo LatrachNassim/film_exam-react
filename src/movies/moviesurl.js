5f0d7649bba14dc2b2cddbc22824f989f77f9133
Nassim <latrach.nassim@gmail.com>
2020-09-02 16:53:20 +0200

    Création de la deuxieme page

---
+++ src/movies/moviesurl.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+
+secondUrl = fetch('http://localhost:3000/movie/2', movieObj = {
+    titre: '',
+    date: '',
+    categories: '',
+    affiche: '',
+    arrierePlan: '',
+    listesActeurs:'',
+    filmsSimilaires:''
+})
+.then(() => {
+    console.log(movieObj);
+})
+.catch((error) => console.error("Ce film n'existe pas"));
+
+
+return(
+    <div>
+        <button onClick={}>Modifier</button>
+        <button onClick={}>Supprimer</button>
+    </div>
+)
+
+export default movieurl
