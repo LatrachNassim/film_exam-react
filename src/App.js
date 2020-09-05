bb9477796ebdaba2fcb361216244b23144e81f5d
Nassim <latrach.nassim@gmail.com>
2020-09-05 14:59:12 +0200

    Creation des routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from "react";
+import addmovies from "./Component/movies/addmovies";
+import moviesfilter from "./Component/movies/moviesFilter";
+import moviesurl from "./Component/movies/moviesurl";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Switch>
+    
+     <Route path="/moviesFilter">< nameForm/>Recherche de film</Route>
+     <Route path="/moviesurl">< /></Route>
+     <Route path="/addmovies">< movie/>Ajout d'un nouveau film</Route>
+     </Switch>;
 }
+
+    
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
 
 export default App;
